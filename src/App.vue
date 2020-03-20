f0fadda0014457a8e2fbfbb22110871037b27cc5
jasonwalsh <rightlag@gmail.com>
2020-03-20 13:01:38 -0400

    feat: begin adding Amazon EBS builder component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AmazonEBS />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AmazonEBS from "./components/AmazonEBS.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    AmazonEBS
   }
 };
 </script>
---
+++ src/components/AmazonEBS.vue
@@ -0,0 +1,30 @@
+<template>
+  <form v-on:submit.prevent>
+    <input v-model="ami_name" type="text" />
+    <button v-on:click="generate">Generate</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      ami_name: "",
+      template: {
+        builders: []
+      }
+    };
+  },
+  methods: {
+    generate: function(event) {
+      console.log(event);
+      let builder = {
+        type: "amazon-ebs"
+      };
+      builder.ami_name = this.ami_name;
+      this.template.builders.push(builder);
+      console.log(this.template);
+    }
+  }
+};
+</script>
